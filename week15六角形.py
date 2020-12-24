ea2173b9c14b918fb10f892fb5f983bacbf898ef
A109260051 <71419535+A109260051@users.noreply.github.com>
2020-12-24 11:12:09 +0800

    Add files via upload

---
+++ week15六角形.py
@@ -0,0 +1,31 @@
+import turtle
+shelly = turtle.Turtle()
+
+turtle.bgcolor("black")
+
+
+for n in range(36):
+
+    colors=["red","yellow","blue","orange","green","purple"]
+    
+    for i in range(6):
+        shelly.color(colors[i])
+        shelly.forward(100)
+        shelly.left(60)
+        
+    shelly.right(10)
+    
+    
+
+shelly.penup()
+shelly.color("white")
+
+for i in range(36):
+    shelly.forward(200)
+    shelly.pendown()
+    shelly.circle(5)
+    shelly.penup()
+    shelly.backward(220)
+    shelly.right(10)
+    
+shelly.hideturtle()
